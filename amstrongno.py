d71f85b92b23cd28e727c8c6822866c1008d0230
nilaypedram <43986474+nilaypedram@users.noreply.github.com>
2018-10-09 15:18:26 +0530

    Add files via upload

---
+++ amstrongno.py
@@ -0,0 +1,15 @@
+def amstrong_number(x):
+   s=0
+   z=x
+   while x>0:
+    y=x%10
+    s=y**3+s
+    x=x//10
+   print(s)
+   if s==z:
+     print('Amstrong no.')
+   else:
+     print('Not an amstrong no.')
+x=int(input())
+result=amstrong_number(x)
+print(result)
