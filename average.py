d44bb32a6605863780798df257e7675347f55100
nilaypedram <43986474+nilaypedram@users.noreply.github.com>
2018-11-27 14:37:59 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,9 +1,15 @@
-total=0
+#title-finding average
+#Nilay Pedram
+#M-45
+
+
+
+total=0                             #initializing
 n=0
 count=0
-while n>=0:
+while n>=0:                         #while loop
    total+=n
-   count+=1
-   n=int(input())
+   count+=1                         #refresh the count variable after one cycle by increasing it by 1
+   n=int(input())                   
 avg=total/(count-1)
 print(avg)
