5e3d7520ce98248f39219c32cd00174900d0a5df
nilaypedram <43986474+nilaypedram@users.noreply.github.com>
2018-11-27 14:43:03 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,14 +1,19 @@
-def factorial(n):
+#title-factorial of a no
+#Nilay Pedram
+#M-45
+
+
+def factorial(n):                                 #defining the fun
    f=1
-   if n<0:
+   if n<0:                                        #if-elif statement
       print('enter positive no.')
    elif n==0 or n==1:
       print('1')
    else:
-      while n>1:
+      while n>1:                                  
        f=f*n
-       n=n-1
+       n=n-1                                      #refreshing the value of n after every one loop until its 1
       print(f)
-n=int(input("enter a no."))
+n=int(input("enter a no."))                       #user input
 result=factorial(n)
 print(result)
