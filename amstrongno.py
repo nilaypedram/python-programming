2a992e151ec08a1763b96047d520fc0d937775d3
nilaypedram <43986474+nilaypedram@users.noreply.github.com>
2018-11-27 14:34:53 +0530

    Update amstrongno.py

--- amstrongno.py
+++ amstrongno.py
@@ -4,18 +4,18 @@
 
 
 
-def amstrong_number(x):
-   s=0
+def amstrong_number(x):                             #function calling
+   s=0                                              #initialising the variables
    z=x
-   while x>0:
+   while x>0:                                       #use of while loop
     y=x%10
     s=y**3+s
     x=x//10
    print(s)
-   if s==z:
+   if s==z:                                         #if condition
      print('Amstrong no.')
    else:
      print('Not an amstrong no.')
 x=int(input())
-result=amstrong_number(x)
+result=amstrong_number(x)                         
 print(result)
